dfaba7c4084e477ce613eac160b2ae2798c240c7
Jasper Reddin <jbreddin@uams.edu>
2022-09-27 22:56:10 -0500

    remove common words from index

--- functions/src/util.ts
+++ functions/src/util.ts
@@ -1,4 +1,10 @@
 export const wordsFromName = (name: string): string[] => {
-  const normalized = name.replaceAll(/\W+/g, ' ').trim().toLowerCase();
-  return normalized.split(' ');
+  const normalized = name
+    .replaceAll('.', '') // clear out periods in case of abbreviations
+    .replaceAll(/\W+/g, ' ') // clear out all non alpha chars
+    .trim()
+    .toLowerCase();
+  return normalized.split(' ')
+    // ignore common words
+    .filter(word => word.length > 2 && word !== 'the');
 };
